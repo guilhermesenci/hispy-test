0b093ce2038d2e1672ba232c5ebc70a717fa129d
Guilherme Senci <guilherme.g.senci@gmail.com>
2024-07-23 03:56:00 -0300

    feat: card component

--- src/app/page.js
+++ src/app/page.js
@@ -1,34 +1,65 @@
-"use client"
+"use client";
 import Image from "next/image";
-import { useEffect } from 'react';
-import supabase from '../lib/supabaseClient';
+import { useEffect, useState } from "react";
+import supabase from "../lib/supabaseClient";
 
 import alertIcon from "../../public/alertIcon.svg";
 
 import NavigationButton from "@/components/navigationButton";
-
-const investigações = [];
+import Card from "@/components/card";
+import Modal from "@/components/modal";
 
 const Home = () => {
+  const [investigation, setInvestigation] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedData, setSelectedData] = useState(null);
+
   useEffect(() => {
     const fetchData = async () => {
-      const { data, error } = await supabase
-        .from('investigacoes')
-        .select('*');
+      try {
+        const { data, error } = await supabase
+          .from("investigacoes")
+          .select("*");
 
-      if (error) {
-        console.error('Error:', error);
-      } else {
-        console.log('Data:', data);
+        if (error) {
+          throw error;
+        }
+
+        if (data) {
+          setInvestigation(data);
+        }
+      } catch (error) {
+        console.error("Erro ao carregar:", error.message);
+      } finally {
+        setLoading(false);
       }
     };
-
     fetchData();
   }, []);
+
+  if (loading) {
+    return (
+      <main className="flex w-full h-[calc(100vh-74px)] items-center justify-center">
+        <p>Carregando...</p>
+      </main>
+    );
+  }
+
+  const openModal = (data) => {
+    setSelectedData(data);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedData(null);
+  };
+
   return (
     <div>
-      <main className="flex w-full h-[calc(100vh-74px)] items-center justify-center">
-        {investigações ? (
+      {investigation.length < 1 ? (
+        <main className="flex w-full h-[calc(100vh-74px)] items-center justify-center">
           <div className="flex flex-col items-center justify-center">
             <Image src={alertIcon} height={68} width={68} alt="alert icon" />
             <p className="text-2xl font-semibold mt-6 mb-3">
@@ -43,10 +74,36 @@ const Home = () => {
               text="Nova investigação"
             />
           </div>
-        ) : (
-          ""
+        </main>
+      ) : (
+        <main className="w-full px-52 mt-10">
+          <div className="flex w-full justify-between">
+            <span className="text-2xl font-semibold">Ivestigações</span>
+            <NavigationButton
+              destiny="newInvestigation"
+              text="Nova investigação"
+            />
+          </div>
+          <div className="mt-8 flex flex-col gap-4 overflow-y-scroll max-h-[500px] pr-2">
+            {investigation.map((item, key) => {
+              return <Card key={key} data={item} onOpenModal={openModal} />;
+            })}
+          </div>
+        </main>
+      )}
+      <Modal isOpen={isModalOpen} onClose={closeModal}>
+        {selectedData && (
+          <div>
+            <h2 className="text-xl font-bold">Informações da Investigação</h2>
+            <p>{selectedData.nome}</p>
+            <p>
+              Criada em:
+              {new Date(selectedData.created_at).toLocaleDateString("pt-BR")}
+            </p>
+            {/* Outros detalhes da investigação */}
+          </div>
         )}
-      </main>
+      </Modal>
     </div>
   );
 };
